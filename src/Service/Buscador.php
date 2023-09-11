ef50feecfe6fafec9fd79e158494d5eb07e0f3bf
Vinicius <viniciuscarrocine@gmail.com>
2023-09-11 06:38:09 -0300

    add PHPUNit and PHPCS

--- src/Service/Buscador.php
+++ src/Service/Buscador.php
@@ -5,28 +5,29 @@ namespace VNCHub\BuscadorDeCursos\Service;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador {
+class Buscador
+{
     private ClientInterface $httpClient;
     private Crawler $crawler;
 
-    public function __construct(ClientInterface $httpClient, Crawler $crawler) {
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
         $this->httpClient = $httpClient;
         $this->crawler = $crawler;
     }
 
-    public function buscarCursos(string $url) : array {
+    public function buscarCursos(string $url): array
+    {
         $request = $this->httpClient->request('GET', $url);
         $html = $request->getBody();
         $this->crawler->addHtmlContent($html);
         $resources = $this->crawler->filter('span.card-curso__nome');
         $courses = array();
 
-        foreach($resources as $resource) {
+        foreach ($resources as $resource) {
             array_push($courses, $resource->textContent);
         }
 
         return $courses;
     }
-
-
 }
